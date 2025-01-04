5ce8fb52dba7b2419091a6ccc5ca9a10c1fc90f7
kishanth29 <139776257+kishanth29@users.noreply.github.com>
2025-01-04 17:14:13 +0530

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index( )
+        {
+            EmployeeBusinessLayer employeeBL = new EmployeeBusinessLayer();
+            Employee employee = employeeBL.GetEmployeeDetails(102);
+            ViewBag.Employee= employee;
+            ViewBag.Header = "Employee Details";
+            return View(employee);
+        }
+    }
+}
---
+++ WebApplication2/Models/EmployeeBusinessLayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class EmployeeBusinessLayer
+    {
+        public Employee GetEmployeeDetails(int EmployeeId)
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = EmployeeId,
+                Name = "Kishanth",
+                Gender = "Male",
+                City = "Jaffna",
+                Salary = 1000,
+                Address = "Andheri"
+
+            };
+            return employee;
+        }
+    }
+}
